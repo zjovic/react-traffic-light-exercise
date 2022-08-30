57f39cd9e236fb22b02024bdfd233e3e721a55ff
zjovic <38418292+zjovic@users.noreply.github.com>
2022-08-30 21:04:38 +0200

    Add lightshow

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -5,6 +5,19 @@ import Light from './Light.jsx'
 const App = () => {
 	const [selected, setSelected] = useState(null);
 	const [colors, setColors] = useState(['red', 'yellow', 'green']);
+	const [lightShow, setLightShow] = useState(false);
+
+	useEffect(() => {
+		if (lightShow) {
+			const interval = setInterval(() => {
+				setSelected((selected) => selected = Math.floor(Math.random() * colors.length));
+			}, 1000);
+	
+			return () => clearInterval(interval);
+		}
+
+	}, [lightShow, colors]);
+
 
 	const lights = colors.map((light, index) =>
 		<Light key={index} index={index} light={light} selected={selected === index} onClickEvent={() => setSelected(index)} />
@@ -16,8 +29,9 @@ const App = () => {
 				{lights}
 			</div>
 			{ colors.includes('purple') === false &&
-				<button onClick={() => setColors([...colors, 'purple']) }> Add purple </button>
+				<button onClick={() => setColors([...colors, 'purple']) }>Add purple</button>
 			}
+			<button onClick={() => setLightShow(!lightShow)}>Lightshow</button>
 		</div>
 	);
 };
