d58397517f96cec53124a8b73fc1457f9886cae2
zjovic <38418292+zjovic@users.noreply.github.com>
2022-08-30 20:41:39 +0200

    Add purple light

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Light from './Light.jsx'
 
 //create your first component
 const App = () => {
 	const [selected, setSelected] = useState(null);
+	const [colors, setColors] = useState(['red', 'yellow', 'green']);
 
-	const lights = ['red', 'yellow', 'green'].map((light, index) =>
+	const lights = colors.map((light, index) =>
 		<Light key={index} index={index} light={light} selected={selected === index} onClickEvent={() => setSelected(index)} />
   	);
 
 	return (
-		<div className="traffic-lights">
-			{lights}
+		<div className="wrapper">
+			<div className="traffic-lights">
+				{lights}
+			</div>
+			{ colors.includes('purple') === false &&
+				<button onClick={() => setColors([...colors, 'purple']) }> Add purple </button>
+			}
 		</div>
 	);
 };
